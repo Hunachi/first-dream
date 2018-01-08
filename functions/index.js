9025b1680184826f33a05e718abf3b41ecefc61f
Hunachi <hanahanahunachi@gmail.com>
2018-01-08 21:58:48 +0900

    Initial commit

---
+++ functions/index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+process.env.DEBUG = 'actions-on-google:*';
+const http = require('request');
+const { DialogflowApp } = require('actions-on-google');
+const functions = require('firebase-functions');
+
+const options = {
+    uri: "https://9399d71d.ngrok.io/google-home-notifier",
+    headers: {
+        "Content-type": "application/x-www-form-urlencoded",
+    },
+    form: {
+        "text": "おやすみ"
+    }
+};
+
+exports.yourAction = functions.https.onRequest((request, response) => {
+    const app = new DialogflowApp({request, response});
+    console.log('Request headers: ' + JSON.stringify(request.headers));
+    console.log('Request body: ' + JSON.stringify(request.body));
+
+    function responseHandler(app) {
+        const speechStr = "おやすみなさい";
+        app.tell(speechStr);
+    }
+
+    const actionMap = new Map();
+
+    //hatsuyume-trigger
+    http.post(options, function(error, response, body){});
+
+    actionMap.set('action.oyasumi', responseHandler);
+    app.handleRequest(actionMap);
+});
+
